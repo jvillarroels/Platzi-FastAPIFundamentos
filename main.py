96e35773f988426242b9619afdcbd172b7684375
Jose Villarroel <josedelvallevs@gmail.com>
2023-08-26 23:06:45 -0500

    Culminacion de la app

--- main.py
+++ main.py
@@ -23,18 +23,3 @@ class Person(BaseModel):
 def home():
     return {"Hello": "World JOSE VILLARROEL"}
 
-# Request and response body
-
-@app.post("/person/new")
-def create_person(person: Person = Body(...)):
-    return person
-
-# Validaciones: Query Parameters
-
-@app.get("/person/detail")
-def show_person(
-#    name: Optional[str] = Query(None, min_length=1, max_length=50),
-    name: Optional[str] = Query(None, min_length=1, max_length=50),
-    age: str = Query(...)
-):
-    return {name: age}
