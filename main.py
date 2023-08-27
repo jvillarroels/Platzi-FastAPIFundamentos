26a645f569fc3d71855a3218a50815c1d9464183
Jose Villarroel <josedelvallevs@gmail.com>
2023-08-26 22:58:19 -0500

    =======> (1) First commit

---
+++ main.py
@@ -0,0 +1,40 @@
+#Python
+from typing import Optional
+
+#Pydantic
+from pydantic import BaseModel
+
+#FastAPI
+from fastapi import FastAPI
+from fastapi import Body, Query
+
+app = FastAPI()
+
+# Models
+
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = None
+
+@app.get("/")
+def home():
+    return {"Hello": "World JOSE VILLARROEL"}
+
+# Request and response body
+
+@app.post("/person/new")
+def create_person(person: Person = Body(...)):
+    return person
+
+# Validaciones: Query Parameters
+
+@app.get("/person/detail")
+def show_person(
+#    name: Optional[str] = Query(None, min_length=1, max_length=50),
+    name: Optional[str] = Query(None, min_length=1, max_length=50),
+    age: str = Query(...)
+):
+    return {name: age}
