f444d9b1900d1975d821389405d5d6d3f5fdb3ed
Jose Villarroel <josedelvallevs@gmail.com>
2023-09-19 13:53:25 -0500

    remove_duplicates

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ class Location(BaseModel):
             }
         }
 
-class Person(BaseModel):
+class PersonBase(BaseModel):
     first_name: str = Field(
         ...,
         min_length=1,
@@ -66,6 +66,8 @@ class Person(BaseModel):
     )
     hair_color: Optional[HairColor] = Field(default=None, example="black")
     is_married: Optional[bool] = Field(default=None, example=False)
+
+class Person(PersonBase):
     password: str = Field(..., min_length=8)
 #class Config:
 #    schema_extra = {
@@ -78,32 +80,12 @@ class Person(BaseModel):
 #        }
 #    }
 
-class PersonOut(BaseModel):
-    first_name: str = Field(
-        ...,
-        min_length=1,
-        max_length=50,
-        example="Miguel"
-    )
-    last_name: str = Field(
-        ...,
-        min_length=1,
-        max_length=50,
-        example="Torres"
-    )
-    age: int = Field(
-        ...,
-        gt=0,
-        le=115,
-        example=25
-    )
-    hair_color: Optional[HairColor] = Field(default=None, example="black")
-    is_married: Optional[bool] = Field(default=None, example=False)
-
+class PersonOut(PersonBase):
+    pass
 
 @app.get("/")
 def home():
-    return {"Hello": "World JOSE VILLARROEL Check-in"}
+    return {"Hello": "World JOSE VILLARROEL Check-in SEGUNDO CURSO"}
 
 # Request and response body
 
