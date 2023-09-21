f93c2c51373de0a80b93271d25d1d9161ed99563
Jose Villarroel <josedelvallevs@gmail.com>
2023-09-20 19:16:59 -0500

    files

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from pydantic import EmailStr
 #FastAPI
 from fastapi import FastAPI
 from fastapi import status
-from fastapi import Body, Query, Path, Form, Header, Cookie
+from fastapi import Body, Query, Path, Form, Header, Cookie, UploadFile, File
 
 #Starlette
 from starlette.status import HTTP_200_OK
@@ -216,3 +216,16 @@ def contact(
 
 ):
     return user_agent
+
+# Files
+@app.post(
+    path="/post-image"
+)
+def post_image(
+    image: UploadFile = File(...)
+):
+    return {
+        "Filename": image.filename,
+        "Format": image.content_type,
+        "Size(kb)": round(len(image.file.read())/1024, ndigits=2)
+    }
